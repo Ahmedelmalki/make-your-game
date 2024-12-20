7a6a0e7e4adf1f47ac18c821642760d8a8128b8d
Ahmedelmalki <elmalkiahmed02@gmail.com>
2024-12-20 20:10:32 +0000

    to be continued

---
+++ main.js
@@ -0,0 +1,22 @@
+// board
+let tileSize = 32;
+let rows = 16;
+let columns = 16;
+
+let board;
+let boardWidth = tileSize * columns; // 32 * 16
+let boardHeight = tileSize * rows; // 32 * 16
+let context;
+
+//ship
+let shipWidth = tileSize * 2;
+let shipHeight = tileSize;
+let shipX = (tileSize * columns) / 2 - tileSize;
+let shipY = tileSize * rows - tileSize * 2;
+
+window.onload = function () {
+  board = document.getElementById("board");
+  board.width = boardWidth;
+  board.height = boardHeight;
+  context = board.getContext("2d");
+};
